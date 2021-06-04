a9a7edfdf09f4cd34702ef5c2cfef0463b21d68b
Mr-ADHD <78214663+Mr-ADHD@users.noreply.github.com>
2021-06-04 15:49:31 +0430

    تغییرات لازم داده شد

--- models.py
+++ models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Users(models.Model):
     # ID use a primary key and random
-    # car_id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField(max_length=8, primary_key=True)
     first_name = models.CharField(max_length=50)
-
     last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50, unique_key=True)
-    password = models.CharField(min_length=8)
-    email = models.CharField(max_length=100, unique_key=True)
-    phone_number = models.CharField(length=11, unique_key=True)
-    house_number = models.CharField(length=11)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=100, unique=True)
+    phone_number = models.CharField(length=11, unique=True)
     gender = models.CharField()
     year_born = models.DateTimeField()
     is_advisor = models.BooleanField()
@@ -19,6 +17,9 @@ class Users(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
 
     # picture???
-    image = models.ImageField()(null=True, blank = True, upload_to = "images/")
-    #for using ImageField you should run this command from the cmd:
+    image = models.ImageField()(null=True, blank=True, upload_to="images/")
+    # for using ImageField you should run this command from the cmd:
     #  pip install pillow
+
+
+
