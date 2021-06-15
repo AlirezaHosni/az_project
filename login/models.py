8bfdb45a1a1a9f659e7682f82b03ec418343de51
ashari0831 <69518520+ashari0831@users.noreply.github.com>
2021-06-15 10:51:58 -0700

    Update models.py
    تغییر برای استفاده از ایمیل به جای یوزرنیم

--- login/models.py
+++ login/models.py
@@ -7,12 +7,20 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
    
+    username = models.CharField(max_length=100,null=True, default=None)
+    email = models.EmailField(unique=True,
+    error_messages={
+            'unique': "A user with that email already exists.",
+        },)
     phone_number = models.CharField(max_length=11, unique=True, null=True)
     gender = models.CharField(max_length=8, null=True)
     year_born = models.DateTimeField(null=True)
     is_advisor = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
 
     # picture???
     image = models.ImageField(null=True, blank=True, upload_to="images/")
