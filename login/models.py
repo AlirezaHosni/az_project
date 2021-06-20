69beb0c66d47d20e81d52a680c57beb7edf5cfdc
ashari0831 <69518520+ashari0831@users.noreply.github.com>
2021-06-20 16:33:35 -0700

    Update models.py

--- login/models.py
+++ login/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 
 
+
 class Manager(UserManager):
     def _create_user(self, email, password, **extra_fields):
         """
@@ -15,7 +16,7 @@ class Manager(UserManager):
         # Lookup the real model class from the global app registry so this
         # manager method can be used in migrations. This is fine because
         # managers are by definition working on the real model.
-        user = self.model(email=email, **extra_fields)
+        user = self.model( email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
@@ -23,7 +24,7 @@ class Manager(UserManager):
     def create_user(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user( email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
@@ -34,30 +35,26 @@ class Manager(UserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user( email, password, **extra_fields)
 
 
 # Create your models here.
 class User(AbstractUser):
+   
     username = None
-    email = models.EmailField(unique=True, null=True,
-                              error_messages={
-                                  'unique': "کاربری با این ایمیل از قبل موجود میباشد",
-                              })
+    email = models.EmailField(unique=True, null=True)
     phone_number = models.CharField(max_length=11, unique=True, null=True,
-                                    error_messages={
-                                        'unique': "کاربری با این شماره تماس از قبل موجود میباشد",
-                                    })
-    gender = models.CharField(choices=(
-        ('m', 'male'),
-        ('f', 'female'),
-    ), max_length=1)
+    error_messages={
+            'unique': "کاربری با این شماره تماس از قبل موجود میباشد",
+        })
+    gender = models.CharField(max_length=8, null=True)
     year_born = models.DateTimeField(null=True)
     is_advisor = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    
 
     # picture???
     image = models.ImageField(null=True, blank=True, upload_to="images/")
@@ -65,26 +62,25 @@ class User(AbstractUser):
     #  pip install pillow
     objects = Manager()
 
-    class Advisor(models.Model):
+class Advisor(models.Model):
         user = models.ForeignKey("User", on_delete=models.CASCADE)
-        is_mental_advisor = models.BooleanField(default=False)
-        is_family_advisor = models.BooleanField(default=False)
-        is_religious_advisor = models.BooleanField(default=False)
-        is_healthcare_advisor = models.BooleanField(default=False)
-        is_ejucation_advisor = models.BooleanField(default=False)
-        meli_code = models.CharField(max_length=1, unique=True, null=True,
+        is_mental_advisor = models.BooleanField(default=False, null=True)
+        is_family_advisor = models.BooleanField(default=False, null=True)
+        is_religious_advisor = models.BooleanField(default=False, null=True)
+        is_healthcare_advisor = models.BooleanField(default=False, null=True)
+        is_ejucation_advisor = models.BooleanField(default=False, null=True)
+        meli_code = models.CharField(max_length=10, unique=True, null=True,
                                      error_messages={
                                          'unique': "کاربری با این کد ملی از قبل موجود میباشد",
                                      })
-
-
+                                     
         advise_method = models.CharField(choices=(
             ('on', 'online'),
             ('off', 'offline'),
             ('b', 'both'),
-        ), max_length=3)
+        ), max_length=3, null=True)
 
-        address = models.CharField("advisor address", max_length=300)
+        address = models.CharField("advisor address", max_length=300, null=True)
 
-        telephone = models.CharField(max_length=11)
-
+        telephone = models.CharField(max_length=11, null=True)
+    
