8419896377620a6ca730ca1141dad2b039eb59b3
ashari0831 <69518520+ashari0831@users.noreply.github.com>
2021-07-05 16:38:46 -0700

    Update urls.py

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginAPI, SignUpAPI, UserInfoAPI, AdvisorInfoAPI
+from .views import AdvisorRequestsInfoAPI, RequestUpdateStatus, RequestsInfoAPI, LoginAPI, SignUpAPI, UserInfoAPI, AdvisorInfoAPI, SearchAdvisorAPI
 from knox import views as knox_views
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
@@ -10,6 +10,10 @@ urlpatterns = [
     path('logout/', knox_views.LogoutView.as_view()),
     path('user-profile/', UserInfoAPI.as_view()),
     path('advisor-profile/', AdvisorInfoAPI.as_view()),
+    path('search/', SearchAdvisorAPI.as_view()),
+    path('user-requests/', RequestsInfoAPI.as_view()),
+    path('update-request-status/<int:pk>/', RequestUpdateStatus.as_view()),
+    path('advisor-requests/', AdvisorRequestsInfoAPI.as_view()),
 
     # YOUR PATTERNS
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
