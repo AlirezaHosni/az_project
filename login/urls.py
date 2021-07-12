22dfd51aead728f1393509e02b76974d504f48b8
ashari0831 <69518520+ashari0831@users.noreply.github.com>
2021-07-11 21:10:16 -0700

    final path

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AdvisorRequestsInfoAPI, RequestUpdateStatus, RequestsInfoAPI, LoginAPI, SignUpAPI, UserInfoAPI, AdvisorInfoAPI, SearchAdvisorAPI
+from .views import ListRateByAdvisorIdAPI, BestAdvisorsByProfessionAPI, GetAllAdvisorsAPI, CreateAdvisor, UpdateAdvisorResumeAPI, ListAdvisorResumeAPI,ListRateAPI, CreateRateAPI, SendRequestAPI, AdvisorRequestsInfoAPI, RequestUpdateStatus, RequestsInfoAPI, LoginAPI, SignUpAPI, UserInfoAPI, AdvisorInfoAPI, SearchAdvisorAPI
 from knox import views as knox_views
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
@@ -12,8 +12,17 @@ urlpatterns = [
     path('advisor-profile/', AdvisorInfoAPI.as_view()),
     path('search/', SearchAdvisorAPI.as_view()),
     path('user-requests/', RequestsInfoAPI.as_view()),
-    path('update-request-status/<int:pk>/', RequestUpdateStatus.as_view()),
+    path('update-request-status/<int:id>/', RequestUpdateStatus.as_view()),
     path('advisor-requests/', AdvisorRequestsInfoAPI.as_view()),
+    path('send-request/', SendRequestAPI.as_view()),
+    path('users-comments/', ListRateAPI.as_view()),
+    path('users-comments-by-advisor-id/', ListRateByAdvisorIdAPI.as_view()),
+    path('create-comment/', CreateRateAPI.as_view()),
+    path('advisor-resume/', ListAdvisorResumeAPI.as_view()),
+    path('update-advisor-resume/<int:granted_prize_id>/', UpdateAdvisorResumeAPI.as_view()),
+    path('create-advisor/', CreateAdvisor.as_view()),
+    path('all-advisors/', GetAllAdvisorsAPI.as_view()),
+    path('particular-advisors/', BestAdvisorsByProfessionAPI.as_view()),
 
     # YOUR PATTERNS
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
