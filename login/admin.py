bd1e0d897a718d62aa7bd5b32d70c346204bca5e
ashari0831 <69518520+ashari0831@users.noreply.github.com>
2021-07-11 21:16:43 -0700

    Update admin.py

--- login/admin.py
+++ login/admin.py
@@ -1,6 +1,10 @@
 
 from django.contrib import admin
-from .models import User
+from .models import Advisor, User, Request, Rate, Advisor_History
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Advisor)
+admin.site.register(Request)
+admin.site.register(Rate)
+admin.site.register(Advisor_History)
