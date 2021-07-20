3b27ad1799451b65729c237f8c0587485d1fcb4e
AlirezaHosni <alireza79hosni@gmail.com>
2021-07-20 18:40:17 +0430

    اضافه کردن ارورهای ثبت نام

--- login/serializer.py
+++ login/serializer.py
@@ -9,44 +9,48 @@ from .models import Advisor, User, Request, Rate, Advisor_History
 from django.contrib.auth.hashers import make_password
 
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','email','password','first_name','last_name','phone_number','gender','year_born','is_advisor','image']
-       
+        fields = ['id', 'email', 'password', 'first_name', 'last_name', 'phone_number', 'gender', 'year_born',
+                  'is_advisor', 'image']
 
     def update(self, instance, validated_data):
         password = validated_data.get('password')
-        
+
         if password is not None:
             super(UserSerializer, self).update(instance, validated_data)
             instance.password = make_password(validated_data.get('password', instance.password))
             instance.save()
         else:
             super(UserSerializer, self).update(instance, validated_data)
-            
+
         return instance
-        
 
 
 class AdvisorSerializer(serializers.ModelSerializer):
     class Meta:
-        model= Advisor
+        model = Advisor
         fields = '__all__'
         read_only_fields = ['user']
 
     def create(self, validated_data):
-        return Advisor.objects.create(user_id=self.context['request'].user.id, 
-            is_mental_advisor = validated_data['is_mental_advisor'],
-            is_family_advisor = validated_data['is_family_advisor'],
-            is_sport_advisor = validated_data['is_sport_advisor'],
-            is_healthcare_advisor = validated_data['is_healthcare_advisor'],
-            is_ejucation_advisor = validated_data['is_ejucation_advisor'],
-            meli_code = validated_data['meli_code'],
-            advise_method = validated_data['advise_method'],
-            address= validated_data['address'],
-            telephone = validated_data['telephone'])
+
+        if validated_data['meli_code'] is not None:
+            isDuplicate_code = Advisor.objects.filter(meli_code=validated_data['meli_code'])
+            if isDuplicate_code.count() >= 1:
+                raise serializers.ValidationError('meli_code')
+
+        return Advisor.objects.create(user_id=self.context['request'].user.id,
+                                      is_mental_advisor=validated_data['is_mental_advisor'],
+                                      is_family_advisor=validated_data['is_family_advisor'],
+                                      is_sport_advisor=validated_data['is_sport_advisor'],
+                                      is_healthcare_advisor=validated_data['is_healthcare_advisor'],
+                                      is_ejucation_advisor=validated_data['is_ejucation_advisor'],
+                                      meli_code=validated_data['meli_code'],
+                                      advise_method=validated_data['advise_method'],
+                                      address=validated_data['address'],
+                                      telephone=validated_data['telephone'])
 
 
 class RegisterSerializer(serializers.Serializer):
@@ -58,8 +62,6 @@ class RegisterSerializer(serializers.Serializer):
     gender = serializers.CharField()
     year_born = serializers.DateTimeField()
     is_advisor = serializers.BooleanField()
-
-
 
     is_mental_advisor = serializers.BooleanField(allow_null=True)
     is_family_advisor = serializers.BooleanField(allow_null=True)
@@ -87,27 +89,46 @@ class RegisterSerializer(serializers.Serializer):
     #     return attrs
 
     def create(self, validated_data):
-        
+
+        duplicates = ''
+
+        isDuplicate_email = User.objects.filter(email=validated_data['email'])
+        if isDuplicate_email.count() >= 1:
+            duplicates = duplicates + 'email and '
+
+        isDuplicate_phone = User.objects.filter(phone_number=validated_data['phone_number'])
+        if isDuplicate_phone.count() >= 1:
+            duplicates = duplicates + 'phon and '
+
+        if validated_data['meli_code'] is not None:
+            isDuplicate_code = Advisor.objects.filter(meli_code=validated_data['meli_code'])
+            if isDuplicate_code.count() >= 1:
+                duplicates = duplicates + 'meli code and '
+
+        if duplicates != '':
+            duplicates = duplicates[:len(duplicates) - 5]
+            raise serializers.ValidationError(duplicates)
+
         user = User.objects.create_user(email=validated_data['email'], password=validated_data['password'],
-            phone_number=validated_data['phone_number'], first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],is_advisor=validated_data['is_advisor'],
-            gender=validated_data['gender'], year_born=validated_data['year_born'], 
-           )
+                                        phone_number=validated_data['phone_number'],
+                                        first_name=validated_data['first_name'],
+                                        last_name=validated_data['last_name'], is_advisor=validated_data['is_advisor'],
+                                        gender=validated_data['gender'], year_born=validated_data['year_born'],
+                                        )
 
         if user.is_advisor == True:
-
             advisor = Advisor.objects.create(user_id=user.id,
-                    is_mental_advisor=validated_data['is_mental_advisor'],
-                    is_family_advisor=validated_data['is_family_advisor'],
-                    is_sport_advisor=validated_data['is_sport_advisor'],
-                    is_healthcare_advisor=validated_data['is_healthcare_advisor'],
-                    is_ejucation_advisor=validated_data['is_ejucation_advisor'],
-                    meli_code=validated_data['meli_code'],
-                    advise_method=validated_data['advise_method'],
-                    address=validated_data['address'],
-                    telephone=validated_data['telephone'])   
+                                             is_mental_advisor=validated_data['is_mental_advisor'],
+                                             is_family_advisor=validated_data['is_family_advisor'],
+                                             is_sport_advisor=validated_data['is_sport_advisor'],
+                                             is_healthcare_advisor=validated_data['is_healthcare_advisor'],
+                                             is_ejucation_advisor=validated_data['is_ejucation_advisor'],
+                                             meli_code=validated_data['meli_code'],
+                                             advise_method=validated_data['advise_method'],
+                                             address=validated_data['address'],
+                                             telephone=validated_data['telephone'])
 
-        return user  
+        return user
 
 
 class SearchInfoSerializer(serializers.Serializer):
@@ -122,7 +143,6 @@ class SearchInfoSerializer(serializers.Serializer):
     gender = serializers.CharField()
     year_born = serializers.DateTimeField()
     image = serializers.ImageField()
-
 
     is_mental_advisor = serializers.BooleanField(allow_null=True)
     is_family_advisor = serializers.BooleanField(allow_null=True)
@@ -143,7 +163,7 @@ class RequestSerializer(serializers.Serializer):
     gender = serializers.CharField()
     image = serializers.ImageField()
 
-    #request model fields
+    # request model fields
     id = serializers.CharField()
     request_content = serializers.CharField()
     is_checked = serializers.BooleanField()
@@ -152,7 +172,6 @@ class RequestSerializer(serializers.Serializer):
     created_at = serializers.DateTimeField()
     is_Done = serializers.BooleanField()
 
- 
 
 class CreateRequestSerializer(serializers.ModelSerializer):
     class Meta:
@@ -161,20 +180,19 @@ class CreateRequestSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Request.objects.create(request_content=validated_data['request_content'],
-            receiver_id=validated_data['receiver'].id, sender_id=self.context['request'].user.id)
-
+                                      receiver_id=validated_data['receiver'].id,
+                                      sender_id=self.context['request'].user.id)
 
 
 class RateSerializer(serializers.ModelSerializer):
-     class Meta:
-         model = Rate
-         fields = ['text', 'rate', 'advisor']
+    class Meta:
+        model = Rate
+        fields = ['text', 'rate', 'advisor']
 
-     def create(self, validated_data):
-         return Rate.objects.create(text=validated_data['text'],
-          rate=validated_data['rate'], user_id=self.context['request'].user.id, 
-          advisor_id=validated_data['advisor'].id)
-
+    def create(self, validated_data):
+        return Rate.objects.create(text=validated_data['text'],
+                                   rate=validated_data['rate'], user_id=self.context['request'].user.id,
+                                   advisor_id=validated_data['advisor'].id)
 
 
 class ListRateSerializer(serializers.Serializer):
@@ -185,36 +203,34 @@ class ListRateSerializer(serializers.Serializer):
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     image = serializers.ImageField()
-    
+
 
 class AdvisorResumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Advisor_History
-        fields = ['id','granted_prize']
+        fields = ['id', 'granted_prize']
 
     def create(self, validated_data):
-         return Advisor_History.objects.create(advisor_id=Advisor.objects.get(user=self.context['request'].user.id).id,
-         granted_prize=validated_data['granted_prize'])
+        return Advisor_History.objects.create(advisor_id=Advisor.objects.get(user=self.context['request'].user.id).id,
+                                              granted_prize=validated_data['granted_prize'])
 
 
 class RequestUpdateSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Request
-        fields = ['is_checked','is_accepted', 'is_blocked', 'sender']
+        fields = ['is_checked', 'is_accepted', 'is_blocked', 'sender']
         read_only_fields = ['sender']
-        
+
 
 class professionFinder(serializers.Serializer):
     profession = serializers.CharField()
-   
+
 
 class RateFinderSerializer(serializers.Serializer):
     advisor_id = serializers.CharField()
 
 
 class AdvisorInfoSerializer(serializers.Serializer):
-    
     email = serializers.EmailField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
@@ -222,7 +238,6 @@ class AdvisorInfoSerializer(serializers.Serializer):
     gender = serializers.CharField()
     year_born = serializers.DateTimeField()
     image = serializers.ImageField()
-
 
     is_mental_advisor = serializers.BooleanField(allow_null=True)
     is_family_advisor = serializers.BooleanField(allow_null=True)
@@ -236,4 +251,3 @@ class AdvisorInfoSerializer(serializers.Serializer):
 
     rate = serializers.CharField()
     advisor_id = serializers.CharField()
-    
