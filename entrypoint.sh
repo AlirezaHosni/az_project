8b676994bd28d5822baef4dbb01b516a57de075b
ashari0831 <69518520+ashari0831@users.noreply.github.com>
2021-12-19 08:15:56 -0800

    change runserver

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 # gunicorn Moshaver.wsgi:application --bind 0.0.0.0:8000
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
 #tail -f /dev/null
