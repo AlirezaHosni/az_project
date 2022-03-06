28d51fa2998b31fe8358329224459d68f53279c3
AlirezaHosni <alireza79hosni@gmail.com>
2022-03-07 02:07:35 +0330

    change query in request

--- login/consumers.py
+++ login/consumers.py
@@ -29,7 +29,7 @@ class RequestConsumer(WebsocketConsumer):
                 # print(self.user_id)
                 self.user = get_object_or_404(User, id=self.user_id)
                 # print('user connected')
-                self.advisor = get_object_or_404(Advisor, id=self.scope['url_route']['kwargs']['advisor_id'])
+                self.advisor = get_object_or_404(Advisor, user_id=self.scope['url_route']['kwargs']['advisor_id'])
                 print(self.advisor.id)
                 self.group_title = self.user.id + self.advisor.id
                 request_content = self.scope['url_route']['kwargs']['request_content']
